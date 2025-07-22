4d7e46855b8dca37c6f3f75bc99d193cea1e3e2d
Anish Kumar Mondal <anish03mondal@gmail.com>
2025-07-22 11:13:57 +0530

    create screen updated

--- lib/create_room_screen.dart
+++ lib/create_room_screen.dart
@@ -10,6 +10,10 @@ class CreateRoomScreen extends StatefulWidget {
 
 class _CreateRoomScreenState extends State<CreateRoomScreen> {
   final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _roomNameController = TextEditingController();
+  late String? _maxRoundsValue;
+  late String? _roomSizeValue;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,6 +32,86 @@ class _CreateRoomScreenState extends State<CreateRoomScreen> {
             child: CustomTextField(
               contorller: _nameController,
               hintText: "Enter your name",
+            ),
+          ),
+          SizedBox(height: 20),
+
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            child: CustomTextField(
+              contorller: _roomNameController,
+              hintText: "Enter your room name",
+            ),
+          ),
+
+          SizedBox(height: 20),
+
+          DropdownButton<String>(
+            focusColor: Color(0xffF5F5FA),
+            items: <String>["2", "5", "10", "15"]
+                .map<DropdownMenuItem<String>>(
+                  (String value) => DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(
+                      value,
+                      style: const TextStyle(color: Colors.black),
+                    ),
+                  ),
+                )
+                .toList(),
+            hint: Text(
+              'Select the number of Rounds',
+              style: TextStyle(
+                fontWeight: FontWeight.w400,
+                fontSize: 14,
+                color: Colors.black,
+              ),
+            ),
+            onChanged: (String? value) {
+              // Handle value change
+              setState(() {
+                _maxRoundsValue = value;
+              });
+            },
+          ),
+          SizedBox(height: 20),
+
+          DropdownButton<String>(
+            focusColor: Color(0xffF5F5FA),
+            items: <String>["2", "3", "4", "5", "6", "7", "8"]
+                .map<DropdownMenuItem<String>>(
+                  (String value) => DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(
+                      value,
+                      style: const TextStyle(color: Colors.black),
+                    ),
+                  ),
+                )
+                .toList(),
+            hint: Text(
+              'Select the room size',
+              style: TextStyle(
+                fontWeight: FontWeight.w400,
+                fontSize: 14,
+                color: Colors.black,
+              ),
+            ),
+            onChanged: (String? value) {
+              // Handle value change
+              setState(() {
+                _roomSizeValue = value;
+              });
+            },
+          ),
+          SizedBox(height: 40),
+
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+            child: Text(
+              "Create",
+              style: TextStyle(fontSize: 16, color: Colors.white),
             ),
           ),
         ],
