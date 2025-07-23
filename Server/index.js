671827d0301fcf874a782994c3298e0e15be81f8
Anish Kumar Mondal <anish03mondal@gmail.com>
2025-07-24 02:09:19 +0530

    get words and room setup

--- Server/index.js
+++ Server/index.js
@@ -6,7 +6,8 @@ const port = process.env.port || 3000;
 //This line creates an HTTP server in Node.js using the http module, and uses an Express app (app) to handle requests.
 var server = http.createServer(app);
 const mongoose = require('mongoose');
-
+const Room = require('./models/Room');
+const getWord = require('./api/getWord');
 //Loads the Socket.IO library and attaches it to your HTTP server.
 var io = require('socket.io')(server);
 
@@ -32,8 +33,32 @@ io.on('connection', (socket) => {
     console.log('Connected');
     socket.on('create-game', async({nickname, name, occupancy, maxRounds}) => {
         try {
+            const existingRoom = await Room.findOne({name});
+            if(existingRoom)
+            {
+                socket.emit('notCorrectGame', 'Room with that name already exists');
+                return;
+            }
+            let room = new Room;
+            let word = getWord();
+            room.word = word;
+            room.nickname = nickname;
+            room.name = name;
+            room.occupancy = occupancy;
+            room.maxRounds = maxRounds;
+
+            let player = {
+                socketID: socket.id,
+                nickname,
+                isPartyLeader: true,
+            }
+            room.players.push(player);
+            room = await room.save();
+            socket.join(room);
+            io.to(name).emit('updateRoom', Room);
             
         } catch (err) {
+            console.log(err);
             
         }
     })
